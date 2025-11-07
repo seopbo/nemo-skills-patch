150b8c85698b4f6886eda7283d0358ea5313901b
Edgar Minasyan <eminasyan@nvidia.com>
2025-11-08 00:20:10 +0400

    return response instead of parsed result

--- nemo_skills/inference/model/base.py
+++ nemo_skills/inference/model/base.py
@@ -268,7 +268,8 @@ class BaseModel:
                     if endpoint_type == EndpointType.chat:
                         assert isinstance(prompt, list), "Chat completion requests must be a list of messages."
                         request_params = self._build_chat_request_params(messages=prompt, stream=stream, **kwargs)
-                        print(f"the request params now are {request_params}\n")
+                        request_params["extra_body"] = kwargs["extra_body"]
+                        print(f"the request_params now are {request_params}")
                         response = await litellm.acompletion(**request_params, **self.litellm_kwargs)
                         if stream:
                             result = self._stream_chat_chunks_async(response)
